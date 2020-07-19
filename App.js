21f7ebc20c0b7503d612876e45f883635e5fa3f1
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-07-19 00:37:07 -0500

    [F-0004] Creating a boilerplate for reducers and sagas

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Router from './src/routes/Router';
+import { Provider } from 'react-redux';
+import Store from './src/redux/store';
+
+const store = Store();
 
 export default function App() {
 	return (
-		<Router />
+		<Provider store={store}>
+			<Router />
+		</Provider>
 	);
 }
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSaga from 'redux-saga';
+import reducers from './../reducers';
+import sagas from './../sagas';
 
 const store = () => {
     const saga = createSaga();
