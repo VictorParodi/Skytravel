4d142a568ad830e10d728689e192f5c564b4f16d
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-07-20 17:52:35 -0500

    [F-0006] Using the Spinner and Grid components from Native Base

--- src/views/LoadingPage/index.js
+++ src/views/LoadingPage/index.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import { Container, Content, Grid, Spinner } from 'native-base';
+import styles from './style';
 import ROUTES from './../../consts';
 
 function LoadingPage({ navigation }) {
     useEffect(() => navigation.navigate(ROUTES.login));
-    return <Text>Loading Page</Text>;
+
+    return (
+        <Container>
+            <Content contentContainerStyle={styles.container}>
+                <Grid style={styles.grid}>
+                    <Spinner color="blue" />
+                </Grid>
+            </Content>
+        </Container>
+    );
 }
 
 export default LoadingPage;
